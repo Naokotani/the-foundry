7a7f9356d96833747c76107f017679de63916b7c
Chris Hughes <naokotani@protonmail.com>
2022-03-23 00:23:27 -0300

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  siteMetadata: {
+    title: `new`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: [{
+    resolve: 'gatsby-source-sanity',
+    options: {
+      "projectId": "bk2ka50u",
+      "dataset": ""
+    }
+  }]
+};
